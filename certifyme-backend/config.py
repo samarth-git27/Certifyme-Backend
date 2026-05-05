bee1c00786f5cb113dbbbefcf21e0864b701c908
Samarth J Bharadwaj <samarthbharadwaj03@gmail.com>
2026-05-05 16:32:44 +0530

    Add configuration settings for the backend

---
+++ certifyme-backend/config.py
@@ -0,0 +1,8 @@
+from datetime import timedelta
+
+class Config:
+    SECRET_KEY = "supersecretkey"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "jwt-secret-key"
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
